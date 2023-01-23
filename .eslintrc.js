8ec9fb18247ef1e34ed0923023c10eed6ba9f092
Reece Dunham <me@rdil.rocks>
2023-01-23 13:37:33 -0500

    Update copyright years to 2023
    Signed-off-by: Reece Dunham <me@rdil.rocks>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 /*
  *     The Peacock Project - a HITMAN server replacement.
- *     Copyright (C) 2021-2022 The Peacock Project Team
+ *     Copyright (C) 2021-2023 The Peacock Project Team
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Affero General Public License as published by
