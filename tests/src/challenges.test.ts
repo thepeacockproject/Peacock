dffe0081a7a9c6e5f7ab4981e3062e16dab6b867
Lennard Fonteijn <lennard@lennardf1989.com>
2023-06-11 14:28:44 +0200

    Added re-usable unit test logic for challenges

--- tests/src/challenges.test.ts
+++ tests/src/challenges.test.ts
@@ -8,12 +8,12 @@ import {
 import {
     ChallengeContext,
     ChallengePackage,
+    SavedChallenge,
 } from "../../components/types/challenges"
-import { LogLevel, log } from "../../components/loggingInterop"
 import { fastClone } from "../../components/utils"
 
 describe("challenges", () => {
-    test("fasterThanYourOwnShadow", async () => {
+    test("fasterThanYourOwnShadow - should fail", async () => {
         const jsonChallengeData = await readFile(
             join(
                 process.cwd(),
@@ -33,76 +33,83 @@ describe("challenges", () => {
                 (e) => e.Id === "5138536b-1c72-459f-b666-96ea0d793c1e",
             )
 
-        console.log(challenge)
-
-        const jsonEventData = await readFile(
-            join(
-                process.cwd(),
-                "tests",
-                "res",
-                "fasterThanYourOwnShadow.session.json",
-            ),
+        const eventDataJsonFile = join(
+            process.cwd(),
+            "tests",
+            "res",
+            "fasterThanYourOwnShadow.session.json",
         )
 
-        const eventData = JSON.parse(
-            jsonEventData.toString(),
-        ) as ClientToServerEvent[]
+        const actual = await testChallenge(challenge, eventDataJsonFile, true)
 
-        const data: ChallengeContext = {
-            context: { AreaIDs: [] },
-            state: "Start",
-            timers: [],
-            timesCompleted: 0,
-        }
+        assert.equal(actual, false)
+    })
+})
 
-        // NOTE: Direct copy from scoreHandler, because saveEvents is not exported and rewire doesn't work with ES6 modules.
-        eventData.forEach((event) => {
-            if (data.state === "Success") {
-                return
-            }
+async function testChallenge(
+    challenge: SavedChallenge,
+    eventDataJsonFile,
+    enableLogging = false,
+) {
+    const jsonEventData = await readFile(eventDataJsonFile)
 
+    const eventData = JSON.parse(
+        jsonEventData.toString(),
+    ) as ClientToServerEvent[]
+
+    const data: ChallengeContext = {
+        context: { AreaIDs: [] },
+        state: "Start",
+        timers: [],
+        timesCompleted: 0,
+    }
+
+    // NOTE: Direct copy from scoreHandler, because saveEvents is not exported and rewire doesn't work with ES6 modules.
+    for (const event of eventData) {
+        enableLogging &&
             console.log(
                 `\n[${event.Timestamp}] ${event.Name}\n${JSON.stringify(
                     event.Value,
                 )}`,
             )
 
-            const options: HandleEventOptions = {
-                eventName: event.Name,
-                currentState: data.state,
-                timers: data.timers,
-                timestamp: event.Timestamp,
-                contractId: "test",
-                logger: (category, message) =>
-                    log(LogLevel.DEBUG, `[${category}] ${message}`),
-            }
+        const options: HandleEventOptions = {
+            eventName: event.Name,
+            currentState: data.state,
+            timers: data.timers,
+            timestamp: event.Timestamp,
+            contractId: "test",
+            logger: enableLogging
+                ? (category, message) => console.log(category, message)
+                : undefined,
+        }
 
-            const previousState = data.state
+        const previousState = data.state
 
-            const result = handleEvent(
-                challenge.Definition as never,
-                fastClone(data.context),
-                event.Value,
-                options,
-            )
+        const result = handleEvent(
+            challenge.Definition as never,
+            fastClone(data.context),
+            event.Value,
+            options,
+        )
 
-            data.state = result.state
-            data.context = result.context || challenge.Definition?.Context || {}
+        data.state = result.state
+        data.context = result.context || challenge.Definition?.Context || {}
 
-            console.log( result.state )
-            
-            if (previousState !== "Failure" && result.state === "Failure") {
-                console.log("Failure!")
+        console.log(result.state)
 
-                return
-            } else if (
-                previousState !== "Success" &&
-                result.state === "Success"
-            ) {
-                console.log("Success!")
+        if (previousState !== "Failure" && result.state === "Failure") {
+            enableLogging && console.log("Failure!")
 
-                return
-            }
-        })
-    })
-})
+            return false
+        } else if (previousState !== "Success" && result.state === "Success") {
+            enableLogging && console.log("Success!")
+
+            return true
+        }
+    }
+
+    enableLogging && console.log("Unknown!")
+
+    return undefined
+}
