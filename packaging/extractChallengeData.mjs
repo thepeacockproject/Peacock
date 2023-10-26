baae32eb86b90b71ba09f4858871283f91081db7
AnthonyFuller <24512050+AnthonyFuller@users.noreply.github.com>
2023-10-26 13:41:22 +0100

    Bump game version

--- packaging/extractChallengeData.mjs
+++ packaging/extractChallengeData.mjs
@@ -71,7 +71,7 @@ async function extract(locationParent, jwt, gameVersion) {
             "User-Agent": "G2 Http/1.0 (Windows NT 10.0; DX12/1; d3d12/1)",
             "Content-Type": "application/json",
             Accept: "application/json, text/*, image/*, application/json",
-            Version: gameVersion === "h1" ? "6.74.0" : "8.13.0",
+            Version: gameVersion === "h1" ? "6.74.0" : "8.14.0",
             Authorization: `bearer ${jwt}`,
         },
     })
