063f6f10c41a4ed2d78d54ad6d46396eb2b2be32
porlium <11916365+prolium@users.noreply.github.com>
2023-11-12 20:42:25 +0300

    Allow the patcher project to open and build on Visual Studio 2022 (#281)

--- patcher/BuildCI.cmd
+++ patcher/BuildCI.cmd
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" patcher\HitmanPatcher.sln -t:Build -p:Configuration=Release -m
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" patcher\HitmanPatcher.sln -t:Build -p:Configuration=Linux.Release -m
+msbuild.exe patcher\HitmanPatcher.sln -t:Build -p:Configuration=Release -m
+msbuild.exe patcher\HitmanPatcher.sln -t:Build -p:Configuration=Linux.Release -m
 rename patcher\bin\x64\Release\PeacockPatcher.exe PeacockPatcher.exe
