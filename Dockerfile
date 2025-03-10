d8d6773bb04b475f34ad1ce01182cfaa08ecc8c9
Joe Ipson <joe@ipson.me>
2025-03-10 14:47:42 -0600

    Move container to be pinned to nvmrc version

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-FROM alpine:3.21.3
+FROM node:22.13.0-alpine3.21
 
-RUN apk add --update nodejs npm su-exec shadow
+RUN apk add --update su-exec shadow
 
 RUN rm -rf /var/cache/apk/*
 
 RUN mkdir /app_build
 WORKDIR /app_build
-RUN npm i -g yarn
 
 COPY . .
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,20 @@ cd /app
 if [ -z "$PUID" ] || [ -z "$PGID" ]; then
   exec node chunk0.js
 else
-  adduser -u $PUID -D abc
-  groupmod -g $PGID abc
+  EXISTING_USER=$(getent passwd "$PUID" | cut -d: -f1)
+  EXISTING_GROUP=$(getent group "$PGID" | cut -d: -f1)
 
-  chown abc:abc -R /app
+  if [ -z "$EXISTING_USER" ]; then
+    adduser -u $PUID -D abc
+    EXISTING_USER=abc
+  fi
 
-  su-exec abc node chunk0.js
+  if [ -z "$EXISTING_GROUP" ]; then
+    groupmod -g $PGID abc
+    EXISTING_GROUP=abc
+  fi
+
+  chown $EXISTING_USER:$EXISTING_GROUP -R /app
+
+  su-exec $EXISTING_USER node chunk0.js
 fi
