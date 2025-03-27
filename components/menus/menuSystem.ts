c977b95726fbcf6702c0e4a43d79e61775c3bd62
AnthonyFuller <24512050+AnthonyFuller@users.noreply.github.com>
2025-03-27 20:51:33 +0000

    feat: bump server versions
    (cherry picked from commit 7b75723ca9c2b8e0bc700740c8cd23eea7cd924d)

--- components/menus/menuSystem.ts
+++ components/menus/menuSystem.ts
@@ -33,7 +33,7 @@ import { SyncBailHook, SyncHook } from "../hooksImpl"
 const menuSystemPreRouter = Router()
 const menuSystemRouter = Router()
 
-// /resources-8-18/
+// /resources-8-19/
 
 /**
  * A class for managing the menu system's fetched JSON data.
