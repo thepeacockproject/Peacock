110fa1e4a643ca95482f6673a26be94d970ab1bd
AnthonyFuller <24512050+AnthonyFuller@users.noreply.github.com>
2025-06-06 23:54:54 +0100

    feat: bump server versions

--- components/index.ts
+++ components/index.ts
@@ -335,7 +335,7 @@ app.use(
                     break
                 case "fghi4567xQOCheZIin0pazB47qGUvZw4":
                 case STEAM_NAMESPACE_2021:
-                    req.serverVersion = "8-19"
+                    req.serverVersion = "8-20"
                     break
                 default:
                     res.status(400).json({ message: "no game data" })
@@ -470,7 +470,7 @@ app.use(
             }
 
             if (
-                ["6-74", "7-3", "7-17", "8-19"].includes(
+                ["6-74", "7-3", "7-17", "8-20"].includes(
                     <string>req.serverVersion,
                 )
             ) {
