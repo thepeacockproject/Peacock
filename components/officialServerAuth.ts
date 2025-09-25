69ec58a76b0716b33d887b02f628d20c96363f89
Govert de Gans <grappigegovert@hotmail.com>
2025-09-25 16:15:32 +0200

    feat: bump server version

--- components/officialServerAuth.ts
+++ components/officialServerAuth.ts
@@ -45,7 +45,7 @@ function createUrlencodedBody(params: Record<string, string>): string {
 
 const requestHeadersH3 = {
     "User-agent": "G2 Http/1.0 (Windows NT 10.0; DX12/1; d3d12/1)",
-    Version: "8.20.0",
+    Version: "8.21.0",
 }
 
 const requestHeadersH2 = {
--- packaging/extractFeaturedContracts.mjs
+++ packaging/extractFeaturedContracts.mjs
@@ -70,7 +70,7 @@ async function extract(jwt, gameVersion) {
             "User-Agent": "G2 Http/1.0 (Windows NT 10.0; DX12/1; d3d12/1)",
             "Content-Type": "application/json",
             Accept: "application/json, text/*, image/*, application/json",
-            Version: gameVersion === "h1" ? "6.74.0" : "8.20.0",
+            Version: gameVersion === "h1" ? "6.74.0" : "8.21.0",
             Authorization: `bearer ${jwt}`,
         },
     })
