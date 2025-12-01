11d6621ad3868f94fba81af472ca236d5e94f62e
Govert de Gans <grappigegovert@hotmail.com>
2025-12-01 11:45:54 +0100

    fix: replace duplicate banker arcade id in MiL with correct Dragon id

--- components/contracts/missionsInLocation.ts
+++ components/contracts/missionsInLocation.ts
@@ -584,7 +584,7 @@ export const missionsInLocation = {
                 "8db8fa33-cb41-4a0d-a2de-e30d884afb95",
                 "9e469023-e0c4-42bf-8527-f9fcaf624421",
             ],
-            LOCATION_BANGKOK: ["80582fdb-c77e-4d6a-b33f-1f14f34b72c4"],
+            LOCATION_BANGKOK: ["3fa83d8d-eb75-4973-926a-6052ee5b2aa4"],
             LOCATION_COLORADO: [
                 "ff5b4e53-49ea-4d85-b94e-d3c8b3fc7ab3",
                 "b4d555eb-e2c9-40c2-b155-328a7019fd28",
