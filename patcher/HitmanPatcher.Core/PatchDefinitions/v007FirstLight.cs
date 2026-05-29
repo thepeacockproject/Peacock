55608e9451d1cf65f2f32fcff9377c05681628d6
Reece Dunham <me@rdil.rocks>
2026-05-28 21:14:27 -0400

    chore: Update for day 1 patch
    https://steamcommunity.com/games/3768760/announcements/detail/693136511334351583
    
    Really scared me for a second there IOI!

--- patcher/HitmanPatcher.Core/PatchDefinitions/v007FirstLight.cs
+++ patcher/HitmanPatcher.Core/PatchDefinitions/v007FirstLight.cs
@@ -4,20 +4,34 @@ namespace HitmanPatcher.PatchDefinitions
     {
         public static void AddVersions()
         {
-            HitmanVersion.AddVersion("007FirstLight", 0x6A135D07, firstLight);
+            HitmanVersion.AddVersion("007FirstLight", 0x6A135D07, firstLightLaunchBuild);
+            HitmanVersion.AddVersion("007FirstLight", 0x6A16C68D, firstLightPatch1);
         }
 
-        private static readonly HitmanVersion firstLight = new HitmanVersion()
+        private static readonly HitmanVersion firstLightLaunchBuild = new()
         {
             // jnz 0x1409b5a73 at VA 0x1409B5BA3 -> RVA 0x9B5BA3 (image base 0x140000000)
-            certpin = new[]
-            {
+            certpin =
+            [
                 new Patch(0x09B5BA3, "0F85", "90E9", MemProtection.PAGE_EXECUTE_READ)
-            },
-            authheader = new Patch[] { },
-            configdomain = new Patch[] { },
-            protocol = new Patch[] { },
-            dynres_noforceoffline = new Patch[] { }
+            ],
+            authheader = [],
+            configdomain = [],
+            protocol = [],
+            dynres_noforceoffline = []
+        };
+
+        private static readonly HitmanVersion firstLightPatch1 = new()
+        {
+            // jnz 0x1409b5bf3 at VA 0x1409B5D23 -> RVA 0x9B5D23 (image base 0x140000000)
+            certpin =
+            [
+                new Patch(0x09B5D23, "0F85", "90E9", MemProtection.PAGE_EXECUTE_READ)
+            ],
+            authheader = [],
+            configdomain = [],
+            protocol = [],
+            dynres_noforceoffline = []
         };
     }
 }
