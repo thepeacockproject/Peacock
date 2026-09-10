51479cb29dca7af4b554935aebd9dde11a79fd61
AnthonyFuller <24512050+AnthonyFuller@users.noreply.github.com>
2026-09-10 21:46:15 +0100

    chore: run prettier

--- components/menus/hub.ts
+++ components/menus/hub.ts
@@ -130,7 +130,12 @@ export function getHubData(gameVersion: GameVersion, userId: string) {
             Name: pack.Name,
             Location: locations.parents["LOCATION_PARENT_ICA_FACILITY"],
             // If IsPack returns false, the tile won't be shown at all.
-            IsPack: (id !== "versus" || !(gameVersion === "h3" && getFlag("hideGhostModeChallengeTile"))),
+            IsPack:
+                id !== "versus" ||
+                !(
+                    gameVersion === "h3" &&
+                    getFlag("hideGhostModeChallengeTile")
+                ),
         }
     }
 
